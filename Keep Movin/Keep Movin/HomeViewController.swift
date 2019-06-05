84223d436a05a87736cb74afcf095340285e94de
Arthur Melo <arthurvelosogm@icloud.com>
2019-06-04 23:48:55 -0400

    Esqueleto da Navigation Bar e Collection view

---
+++ Keep Movin/Keep Movin/HomeViewController.swift
@@ -0,0 +1,99 @@
+//
+//  FirstViewController.swift
+//  Keep Movin
+//
+//  Created by Arthur Melo on 04/06/19.
+//  Copyright © 2019 Keep Movin'. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let helloText = "Olá, Arthur"
+    let message = "Sua atividade"
+    let navBarColor = UIColor(red: 217/255, green: 4/255, blue: 41/255, alpha: 1.0)
+    let cellColor = UIColor(red: 239/255, green: 35/255, blue: 60/255, alpha: 1.0)
+    
+    @IBOutlet weak var cardsCollectionView: UICollectionView!
+    
+    let performanceCellId = "performance"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        setupNavBar()
+        cardsCollectionView.register(PerformanceCell.self, forCellWithReuseIdentifier: performanceCellId)
+        
+    }
+    
+    func setupNavBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .automatic
+        
+        self.title = helloText + "\n" + message
+        
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font : UIFont.preferredFont(forTextStyle: .largeTitle)
+        ]
+        
+        self.navigationController?.navigationBar.barTintColor = navBarColor
+        
+        for navItem in(navigationController?.navigationBar.subviews)! {
+            for itemSubView in navItem.subviews {
+                if let largeLabel = itemSubView as? UILabel {
+                    largeLabel.text = self.title
+                    largeLabel.numberOfLines = 2
+                }
+            }
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: performanceCellId, for: indexPath) as! PerformanceCell
+        cell.backgroundColor = cellColor
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 37, bottom: 0, right: 37)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 350)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 50
+    }
+}
+
+class PerformanceCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.layer.cornerRadius = 20.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 5.0)
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOpacity = 0.5
+        self.layer.cornerRadius = 20
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
