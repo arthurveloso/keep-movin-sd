a2c87e534f7f95dc1e841c31045c724ac521ffda
nickson kley <nicksonkley14@gmail.com>
2019-06-18 09:32:53 -0400

    firebase pod instal

--- Keep Movin/Keep Movin/AppDelegate.swift
+++ Keep Movin/Keep Movin/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,8 +22,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let tabBar = self.window?.rootViewController as? UITabBarController {
             tabBar.selectedIndex = 1
         }
+        
+        FirebaseApp.configure()
+        
         return true
     }
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
